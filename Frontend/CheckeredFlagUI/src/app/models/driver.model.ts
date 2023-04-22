7fc8dd94956981714d147e117545e1e8b102141f
Alen <gameralen2002@gmail.com>
2023-04-22 20:02:57 +0200

    Maquetacion terminada del SelectedDriver faltan estilos

--- Frontend/CheckeredFlagUI/src/app/models/driver.model.ts
+++ Frontend/CheckeredFlagUI/src/app/models/driver.model.ts
@@ -7,7 +7,9 @@ export class Driver {
   flag:string;
   number:number;
   imageDriver:string;
-  marketValue:number;
+  seasonStartPrice:number;
+  currentPrice:number;
+  seasonChange:number;
   team:number;
 
   constructor(){
@@ -17,7 +19,9 @@ export class Driver {
     this.age=0;
     this.country='';
     this.flag='';
-    this.marketValue=0;
+    this.currentPrice=0;
+    this.seasonStartPrice=0;
+    this.seasonChange=0;
     this.imageDriver='';
     this.team=0;
     this.number=0;
