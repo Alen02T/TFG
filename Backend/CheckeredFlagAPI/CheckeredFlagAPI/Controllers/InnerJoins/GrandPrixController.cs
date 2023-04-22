038e5135590211a39ce6b838383da32540c299ca
Alen <gameralen2002@gmail.com>
2023-04-23 00:57:12 +0200

    GrandPrix inner join added

---
+++ Backend/CheckeredFlagAPI/CheckeredFlagAPI/Controllers/InnerJoins/GrandPrixController.cs
@@ -0,0 +1,95 @@
+﻿using CheckeredFlagAPI.Data;
+using CheckeredFlagAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheckeredFlagAPI.Controllers.InnerJoins
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GrandPrixController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public GrandPrixController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+
+        // List<Circuit> circuit= new List<Circuit>();
+        //List<Race> race = new List<Race>();
+
+        // api/GrandPrix/GetGrandPrix
+        [HttpGet]
+        public ActionResult<List<GrandPrix>> GetAllGrandPrix()
+        {
+            //var _context = new UserRegistrationContext();
+            var grandprixList = (from r in _context.Races
+                                 join c in _context.Circuits on r.Circuit equals c.circuitId
+                                 join s in _context.Sponsors on r.Sponsor equals s.sponsorId
+
+                                 select new GrandPrix()
+                                 {
+                                     RaceId = r.Id,
+
+                                     CircuitName = c.name,
+                                     CircuitCountry = c.country,
+                                     CircuitFlag = c.flag,
+                                     CircuitImageMap = c.imageMap,
+                                     CircuitLaps = c.laps,
+                                     CircuitImage = c.imageCircuit,
+
+                                     RaceYear = r.year,
+                                     RaceRound = r.round,
+                                     RaceName = r.name,
+                                     RaceDate = r.date,
+
+                                     SponsorName = s.Name,
+                                     SponsorLink = s.Link,
+
+                                 }).ToList();
+            return grandprixList;
+        }
+
+
+
+
+        [HttpGet("{id}")]
+        public ActionResult<GrandPrix> GetGrandPrixById(int id)
+        {
+            //var _context = new UserRegistrationContext();
+            var grandprixList = (from r in _context.Races
+                                 join c in _context.Circuits on r.Circuit equals c.circuitId
+                                 join s in _context.Sponsors on r.Sponsor equals s.sponsorId
+                                 where r.round == id
+                                 select new GrandPrix()
+                                 {
+                                     RaceId = r.Id,
+
+                                     CircuitName = c.name,
+                                     CircuitCountry = c.country,
+                                     CircuitFlag = c.flag,
+                                     CircuitImageMap = c.imageMap,
+                                     CircuitLaps = c.laps,
+                                     CircuitImage = c.imageCircuit,
+                                     CircuitdriverRecord = c.driverRecord,
+                                     CircuitlapRecord  =c.lapRecord,
+                                     Circuitlength=c.length,
+
+                                     RaceYear = r.year,
+                                     RaceRound = r.round,
+                                     RaceName = r.name,
+                                     RaceDate = r.date,
+
+                                     SponsorName = s.Name,
+                                     SponsorLink = s.Link,
+
+
+
+                                 }).FirstOrDefault();
+            return Ok(grandprixList);
+        }
+    }
+}
---
+++ Backend/CheckeredFlagAPI/CheckeredFlagAPI/Models/GrandPrix.cs
@@ -0,0 +1,29 @@
+﻿namespace CheckeredFlagAPI.Models
+{
+    public class GrandPrix
+    {
+        public int RaceId { get; set; }
+
+        public string CircuitName { get; set; }
+        public string CircuitCountry { get; set; }
+        public string CircuitFlag { get; set; }
+        public string CircuitImageMap { get; set; }
+        public int CircuitLaps { get; set; }
+        public string CircuitImage { get; set; }
+
+        public int Circuitlength { get; set; }
+        //Informacion historica
+        public string CircuitdriverRecord { get; set; }
+        public string CircuitlapRecord { get; set; }
+
+        public int RaceYear { get; set; }
+        public int RaceRound { get; set; }
+        //public int RaceCircuit { get; set; }
+        public string RaceName { get; set; }
+        public string RaceDate { get; set; }
+
+        public string SponsorName { get; set; }
+
+        public string SponsorLink { get; set; }
+    }
+}
