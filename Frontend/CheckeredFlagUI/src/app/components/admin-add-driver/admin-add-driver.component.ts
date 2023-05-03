6b5d6111c5dff4b5711a47a10aead24c3a8729d8
Alen <gameralen2002@gmail.com>
2023-05-03 23:10:53 +0200

    Cambios en el AddDriver

--- Frontend/CheckeredFlagUI/src/app/components/admin-add-driver/admin-add-driver.component.ts
+++ Frontend/CheckeredFlagUI/src/app/components/admin-add-driver/admin-add-driver.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Driver } from 'src/app/models/driver.model';
 import { Team } from 'src/app/models/team.model';
@@ -11,13 +11,16 @@ import { TeamService } from 'src/app/services/team.service';
   styleUrls: ['./admin-add-driver.component.css']
 })
 export class AdminAddDriverComponent implements OnInit {
-
+  @ViewChild('owrap', {static: true}) miElemento: ElementRef | any;
 
   driver:Driver | null;
   team:Team | null;
   nombre:string;
 
   pilotForm: FormGroup; // Define la variable del formulario
+
+  imagenElegida:string;
+
 
 
 
@@ -27,6 +30,9 @@ export class AdminAddDriverComponent implements OnInit {
     this.driver=null;
     this.team=null;
 
+      this.miElemento=ElementRef;
+
+    this.imagenElegida="";
     this.nombre=""
 
     this.pilotForm = this.formBuilder.group({
@@ -34,10 +40,10 @@ export class AdminAddDriverComponent implements OnInit {
       name: [null, Validators.required],
       lastname: [null, Validators.required],
       age: [null, Validators.required],
-      country: [null, Validators.required],
+      country: ['España', Validators.required],
       flag: ['es', Validators.required],
       number: [null, Validators.required],
-      imageDriver: [null, Validators.required],
+      imageDriver: ["", Validators.required],
       seasonStartPrice: [800000, Validators.required],
       currentPrice: [800000, Validators.required],
       seasonChange: [0, Validators.required],
@@ -49,12 +55,25 @@ export class AdminAddDriverComponent implements OnInit {
 
 
 
+  obtenerSrcImagen(string:any) {
+    this.imagenElegida=string;
+    console.log(this.imagenElegida)
+    this.pilotForm.controls['imageDriver'].setValue(this.imagenElegida);
+  }
+
+
+
 
   submitForm(){
     console.log("Se ha añadido")
   }
 
-  ngOnInit(): void {
+
+
+
+  ngOnInit() {
 
   }
 }
+
+
